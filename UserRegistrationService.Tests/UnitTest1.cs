02ed7920fce177a3698ad13a8c87c6c113ed3faa
ChrisLaptop <ChristosBoukasT@gmail.com>
2024-04-01 15:23:34 +0200

    cleaned up and added comments to user name registration test

--- UserRegistrationService.Tests/UnitTest1.cs
+++ UserRegistrationService.Tests/UnitTest1.cs
@@ -4,19 +4,27 @@ namespace userregistrationservice.tests;
 public class UnitTest1
 {
     [TestMethod]
-    public void RegisterName()
+    public void ValidateInputForUsernameRegistration_ShouldThrowExceptionsForInvalidInputAndReturnsTrueForValidInput()
     {
-        // Arrange
+        // Arrange variations of strings
         UserRegistrationService service = new UserRegistrationService();
-        string numericValuesExceedingLimit = "OnlyAlphaNumericChars0123456789";
-        string numericValuesConceedingLimit = "cl12";
+        string numericValuesExceedingCharLimit = "OnlyAlphaNumericChars0123456789";
+        string numericValuesConceedingCharLimit = "cl12";
         string specialCharacters = "SpecialCharacters#!£€{";
         string numericCharactersWithinRange = "Character12345";
 
         // Act and Assert
-        Assert.ThrowsException<ArgumentException>(() => service.RegisterUserName(numericValuesExceedingLimit), "Invalid number of characters");
-        Assert.ThrowsException<ArgumentException>(() => service.RegisterUserName(numericValuesConceedingLimit), "Invalid number of characters");
+
+        // Check if exception is thrown when string exceeding character limit
+        Assert.ThrowsException<ArgumentException>(() => service.RegisterUserName(numericValuesExceedingCharLimit), "Invalid number of characters");
+
+        // Check if exception is thrown when string concceeding character limit
+        Assert.ThrowsException<ArgumentException>(() => service.RegisterUserName(numericValuesConceedingCharLimit), "Invalid number of characters");
+
+        // Check if exception is thrown when string contains special characters
         Assert.ThrowsException<ArgumentException>(() => service.RegisterUserName(specialCharacters), "Contains special characters");
+
+        // Checks if method returns true if validation succeeds
         Assert.IsTrue(service.RegisterUserName(numericCharactersWithinRange));
     }
 }
