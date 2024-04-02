7abe2908e2ba35607a217b43344ed50719f14028
Christos Boukas <Christosboukast@gmail.com>
2024-04-02 14:01:56 +0200

    -added tests for validating new and duplicate users

--- UserRegistrationService.Tests/CreateUserTests.cs
+++ UserRegistrationService.Tests/CreateUserTests.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using System.Xml.Linq;
 using UserRegistrationService;
 
 namespace userregistrationservice.tests;
@@ -6,15 +8,47 @@ namespace userregistrationservice.tests;
 public class CreateUserTests
 {
     [TestMethod]
-    public void IsUniqueUsernameVerifyingUniqueUnexistingUsername_ShouldReturnTrue()
+    public void CreateAndAddNewValidUser_ShouldAddNewUserToUserListAndReturnConfirmationMessage()
+    { 
+        // Arrange
+        UserRegistrationService service = new UserRegistrationService();
+
+        // Act
+        string confirmationMessage = service.CreateUser("ValidUsername", "ValidPass", "Valid@gmail.com");
+
+        // Assert that a user has been added to the service.Users list
+        Assert.AreEqual(1, service.Users.Count);
+
+        // Assert that correct information has been stored to the users list
+        Assert.AreEqual("ValidUsername", service.Users[0].Username);
+        Assert.AreEqual("ValidPass", service.Users[0].Password);
+        Assert.AreEqual("Valid@gmail.com", service.Users[0].Email);
+
+        // Assert that confirmation message is received
+        Assert.AreEqual("User ValidUsername added successfully", confirmationMessage);
+    }
+
+    [TestMethod]
+    public void TryToAddInvalidUserWithDuplicateUsername_ShouldThrowExceptionAndKeepOriginalValuesInUsersList()
     {
-        //Arrange
-        UserRegistrationService userRegistration = new UserRegistrationService();
+        // Arrange
+        UserRegistrationService service = new UserRegistrationService();
+        service.CreateUser("Username", "OGPassword", "OGEmail@gmail.com");
 
-        //Act
-        bool usernameIsUnique = userRegistration.ValidateUniqueUsername("newUniqueUsername");
+        // Store the original user details
+        string originalUsername = service.Users[0].Username;
+        string originalPassword = service.Users[0].Password;
+        string originalEmail = service.Users[0].Email;
 
-        //Assert
-        Assert.IsTrue(usernameIsUnique);
+        // Act create new non-unique user and Assert that exception is thrown when username is not unique
+        Assert.ThrowsException<ArgumentException>(() => service.CreateUser("Username", "NewPassword", "NewEmail@gmail.com"), "Username is not unique.");
+
+        // Assert that new user has not been added to the service.Users list
+        Assert.AreEqual(1, service.Users.Count);
+
+        // Assert that the original user remains the same in the service.Users lists (e.g that the properties are not overwritten)
+        Assert.AreEqual(originalUsername, service.Users[0].Username);
+        Assert.AreEqual(originalPassword, service.Users[0].Password);
+        Assert.AreEqual(originalEmail, service.Users[0].Email);
     }
 }
